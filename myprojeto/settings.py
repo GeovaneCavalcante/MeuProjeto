2f3e8d98dfe272a7505337eadb4cf52e18228bb9
GeovaneDevelop <geovanefeitosacavalcante@gmail.com>
2017-02-27 14:02:34 -0300

    geovane

--- myprojeto/settings.py
+++ myprojeto/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT = BASE_DIR + '/media'
 
 MEDIA_URL = '/media/'
@@ -124,6 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
